244b1646ec4d87ecf47d0ea8a1fe35752c687660
Pranay <pranay.agra@gmail.com>
2023-01-16 21:04:47 -0800

    HQ

--- src/basicbot/robots/HeadQuarters.java
+++ src/basicbot/robots/HeadQuarters.java
@@ -73,7 +73,7 @@ public class HeadQuarters extends Robot {
     Communicator.clearEnemyComms();
 
     if (Cache.PerTurn.ROUND_NUM == 10) {
-      Printer.print("HQ" + hqID + "--" + totalSpawns + "--");
+      Printer.print("HQ" + Cache.Permanent.OUR_TEAM + hqID + "--" + totalSpawns + "--");
     }
 
     if (Cache.PerTurn.ROUND_NUM == 20) {
