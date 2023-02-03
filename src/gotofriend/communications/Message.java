99f2f13d18f45a589d30caeb906256a2e823119f
Alex Kim <alexkimt@gmail.com>
2023-02-02 23:33:32 -0500

    pakage gotofriend

---
+++ src/gotofriend/communications/Message.java
@@ -0,0 +1,4 @@
+package gotofriend.communications;
+
+public class Message {
+}
---
+++ src/gotofriend/knowledge/unitknowledge/UnitKnowledge.java
@@ -0,0 +1,16 @@
+package gotofriend.knowledge.unitknowledge;
+
+import gotofriend.knowledge.Cache;
+import gotofriend.knowledge.Memory;
+import gotofriend.robots.pathfinding.unitpathing.*;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotType;
+
+public abstract class UnitKnowledge {
+  public static MapLocation[] newUnseenMapLocations(MapLocation prev, MapLocation curr) {
+    return Memory.uk.newUnseenMapLocationsImpl(prev, curr);
+  }
+
+
+  protected abstract MapLocation[] newUnseenMapLocationsImpl(MapLocation prev, MapLocation curr);
+}
